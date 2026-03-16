0b5f457ae40033c20b326e307717d2d8ee8b4f7c
Mohana Krishna Chaganti <kmohana@novainternationaldesigns.com>
2026-03-16 13:04:03 -0500

    fix: add noopener noreferrer to social media links - prevent reverse tabnabbing

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
       <p>© Nova International Designs Corporation. All rights reserved.</p>
 
       <div className="social-icons">
-        <a href="https://www.facebook.com/profile.php?id=61584196192112" target="_blank"><img src="/images/facebook-logo.png" alt="Facebook" /></a>
-        <a href="https://www.instagram.com/novainternationaldesigns/" target="_blank"><img src="/images/instagram-logo.png" alt="Instagram" /></a>
-        <a href="https://www.linkedin.com/in/nova-international-designs-corporation-9188333a0/" target="_blank"><img src="/images/linkedIn-logo.png" alt="LinkedIn" /></a>
-        <a href="#" target="_blank"><img src="/images/x_logo.png" alt="X" /></a>
+        <a href="https://www.facebook.com/profile.php?id=61584196192112" target="_blank" rel="noopener noreferrer"><img src="/images/facebook-logo.png" alt="Facebook" /></a>
+        <a href="https://www.instagram.com/novainternationaldesigns/" target="_blank" rel="noopener noreferrer"><img src="/images/instagram-logo.png" alt="Instagram" /></a>
+        <a href="https://www.linkedin.com/in/nova-international-designs-corporation-9188333a0/" target="_blank" rel="noopener noreferrer"><img src="/images/linkedIn-logo.png" alt="LinkedIn" /></a>
+        <a href="#" target="_blank" rel="noopener noreferrer"><img src="/images/x_logo.png" alt="X" /></a>
       </div>
     </div>
   );
